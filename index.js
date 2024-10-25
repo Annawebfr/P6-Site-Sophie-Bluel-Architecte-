6d8dbbbf6807b0814c895ff34761879f22351c9b
Anna Hultgren <annahultgren5@gmail.com>
2024-10-25 14:06:26 +0200

    chg de codes

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ function displayAdminElements() {
 
 //   /**textElement.addEventListener("click", () => {
 //     displayContainerModals(); // Assurez-vous que cette fonction existe
-//   });
+//   });**/
 // }
 
 // Fonction pour créer un élément i
@@ -229,30 +229,3 @@ const createIconElement = (className) => {
 
 // Appel de la fonction pour charger la page
 loadingPage();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
